ef99154ba75c65822abbd3525e8b1b1fb1ada8d3
Hadi493 <hadialam493@gmail.com>
2025-09-28 10:51:45 +0600

    ...

---
+++ qutebrowser/config.py
@@ -0,0 +1,17 @@
+import catppuccin
+
+config.load_autoconfig()
+
+catppuccin.setup(c, 'mocha', True)
+
+# A dark mode for qutebrowser with adblocking enabled
+config.set("colors.webpage.darkmode.enabled", True)
+
+config.set("content.blocking.method", "both")
+
+# Disable dark mode for specific websites
+config.set("colors.webpage.darkmode.enabled", False, "https://chatgpt.com/*")
+
+config.set("colors.webpage.darkmode.enabled", False, "https://chatgpt.com/*")
+config.set("colors.webpage.darkmode.enabled", False, "https://github.com/*")
+config.set("colors.webpage.darkmode.enabled", False, "https://discord.com/*")
