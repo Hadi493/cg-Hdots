7fe1132e0add318464870a077250dee378df0887
Hadi493 <hadialam493@gmail.com>
2025-10-15 23:18:40 +0600

    fix: special for archlinux

--- configs/polybar/scripts/temperature.sh
+++
@@ -1,5 +0,0 @@
-
-#!/bin/bash
-# Get CPU temperature (adjust 'coretemp' based on `sensors` output)
-temp=$(sensors | grep 'Package id' | awk '{print $4}' | tr -d '+°C')
-echo " $temp°C"  #  is a thermometer icon (Nerd Fonts)
